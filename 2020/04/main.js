b4038450b1517f2e0afc2225bad067e6d4b35449
adrian <16654653+adaviloper@users.noreply.github.com>
2020-12-04 00:37:55 -0600

    2020-12-04

---
+++ 2020/04/main.js
@@ -0,0 +1,34 @@
+const input = require('./data.js').input;
+
+const validPassportsForPart1 = input.filter(passport => Object.keys(passport).length > 6)
+  .filter(passport => {
+    return Object.keys(passport).length === 8 || passport.cid === undefined
+  })
+
+const validEyeColors = {
+  amb: 'amb',
+  blu: 'blu',
+  brn: 'brn',
+  gry: 'gry',
+  grn: 'grn',
+  hzl: 'hzl',
+  oth: 'oth',
+}
+
+const validPassportsForPart2 = validPassportsForPart1
+  .filter(passport => passport.byr >= 1920 && passport.byr <= 2002)
+  .filter(passport => passport.iyr >= 2010 && passport.iyr <= 2020)
+  .filter(passport => passport.eyr >= 2020 && passport.eyr <= 2030)
+  .filter(passport => {
+    const unit = passport.hgt.substr(-2);
+    const value = parseInt(passport.hgt.replace(unit, ''));
+    if (unit === 'cm') {
+      return value >= 150 && value <= 193;
+    }
+    return value >= 59 && value <= 76;
+  })
+  .filter(passport => (new RegExp('^(#[0-9a-f]{6})$')).test(passport.hcl))
+  .filter(passport => validEyeColors[passport.ecl])
+  .filter(passport => (new RegExp('^([0-9]{9})$')).test(passport.pid))
+
+console.log(validPassportsForPart1.length, validPassportsForPart2.length);
