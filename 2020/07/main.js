3c3284a379c961d6c7482b62ce19078cfe8982b2
adrian <16654653+adaviloper@users.noreply.github.com>
2020-12-08 23:19:41 -0600

    2020-12-07

---
+++ 2020/07/main.js
@@ -0,0 +1,62 @@
+const input = require('./data').input;
+
+const tap = (data, callback) => {
+  callback(data);
+  return data;
+};
+
+const convertToBags = (rule) => {
+  return rule.split(', ')
+    .map(bagRule => {
+      const quantity = parseInt(bagRule.replace(/[ a-zA-Z]*/g, '')) || 0;
+      const label = bagRule.replace(/(^[0-9]*)/g, '').trim();
+      return {
+        quantity,
+        label,
+      }
+    });
+}
+
+const bags = input.reduce((accumulator, rule) => {
+  const [label, contains] = rule.replace(/( bags?|\.)/g, '').split(' contain ')
+  const subBags = convertToBags(contains);
+  return tap(accumulator, (bags) => {
+    bags[label] = subBags;
+  });
+}, {});
+
+const labels = Object.keys(bags);
+
+const contains = (label, target) => {
+  let containTarget = [];
+  if (bags[label]) {
+    if (bags[label].some(bag => bag.label === target)) {
+      return true;
+    }
+
+    containTarget = bags[label].filter(bag => {
+      return contains(bag.label, target);
+    });
+  }
+
+  return !!containTarget.length;
+}
+
+const containShinyGold = labels.filter(label => {
+  return contains(label, 'shiny gold');
+}).reduce((accumulator, label) => {
+  return tap(accumulator, (labels) => {
+    labels[label] =label;
+  })
+}, {})
+
+const requiredCount = (label) => {
+  if (label === 'no other') {
+    return 0;
+  }
+  return bags[label].reduce((accumulator, bag) => {
+    return accumulator + bag.quantity + bag.quantity * requiredCount(bag.label);
+  }, 0);
+}
+
+console.log('main.js@:44', Object.keys(containShinyGold).length, requiredCount('shiny gold'));
