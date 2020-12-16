36932be5ab5ae7f883019e9a9dcf6b0910b73ba1
adrian <16654653+adaviloper@users.noreply.github.com>
2020-12-15 18:12:43 -0600

    2020-12-12

---
+++ 2020/13/main.1.js
@@ -0,0 +1,17 @@
+const input = require('./data').input;
+
+class Bus {
+  constructor(headings) {
+  }
+
+  depart() {}
+}
+
+const earliestPossibleDeparture = input[0];
+
+input[1].map()
+
+const ship = new Bus();
+ship.depart();
+
+console.log('main.js@:24', earliestPossibleDeparture);
