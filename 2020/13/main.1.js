30dc93f7f52c7424c647e9973dba4ee000bce7bb
adrian <16654653+adaviloper@users.noreply.github.com>
2020-12-20 14:45:06 -0600

    removed dead code

--- 2020/13/main.1.js
+++ 2020/13/main.1.js
@@ -1,36 +1,14 @@
 const input = require('./data').input;
 
-const earliestPossibleDeparture = input[0];
-
-class Bus {
-  constructor(id) {
-    this.id = id;
-    this.departsAt = 0;
-  }
-
-  setNextDepartureAfter() {
-    this.departsAt = Math.ceil(earliestPossibleDeparture / this.id) * this.id;
-    return this;
-  }
-
-  waitTime() {
-    return this.departsAt - earliestPossibleDeparture;
-  }
-}
-
-let fleet = input[1].split(',')
-  .filter(busId => busId !== 'x')
-  .map(busId => {
-    return new Bus(parseInt(busId));
+const fleet = input[1].split(',')
+  .map((id, index) => {
+    if (id === 'x') {
+      return null;
+    }
+    return {
+      id: parseInt(id),
+      dt: index,
+    }
   })
 
-fleet = fleet.map(bus => {
-  return bus.setNextDepartureAfter(earliestPossibleDeparture);
-}).sort((a, b) => {
-  return a.departsAt - b.departsAt;
-})
-
-const bus = fleet[0];
-const solution = bus.waitTime() * bus.id;
-
-console.log('main.js@:24', solution);
+console.log('main.js@:24', fleet);
--- 2020/13/main.2.js
+++ 2020/13/main.2.js
@@ -1,95 +1,36 @@
 const input = require('./data').input;
 
-class WayPoint {
-  constructor(props) {
-    this.rotation = 0;
-    this.distances = {
-      N: props.N,
-      E: props.E,
-      S: props.S,
-      W: props.W,
-    };
+const earliestPossibleDeparture = input[0];
+
+class Bus {
+  constructor(id) {
+    this.id = id;
+    this.departsAt = 0;
   }
 
-  rotate(heading) {
-    const compass = {
-      '0': 'E',
-      '90': 'N',
-      '180': 'W',
-      '270': 'S',
-    };
-    const currentDistances = { ...this.distances };
-    const dh = (this.rotation + (heading.multiplier * heading.distance) + 360) % 360;
-    Object.keys(compass).forEach(direction => {
-      const compassChange = (parseInt(direction) + (heading.multiplier * heading.distance) + 360) % 360;
-      this.distances[compass[compassChange]] = currentDistances[compass[direction]];
-    });
+  setNextDepartureAfter() {
+    this.departsAt = Math.ceil(earliestPossibleDeparture / this.id) * this.id;
+    return this;
   }
 
-  translate(heading) {
-    this.distances[heading.direction] += heading.distance;
+  waitTime() {
+    return this.departsAt - earliestPossibleDeparture;
   }
 }
 
-class Ship {
-  constructor(headings) {
-    this.headings = headings;
-    this.wayPoint = new WayPoint({ N: 1, E: 10, W: 0, S: 0 })
-    this.distances = {
-      N: 0,
-      E: 0,
-      S: 0,
-      W: 0,
-    };
-  }
+let fleet = input[1].split(',')
+  .filter(busId => busId !== 'x')
+  .map(busId => {
+    return new Bus(parseInt(busId));
+  })
 
-  sail() {
-    this.headings.forEach(heading => {
-      if (heading.command === 'propel') {
-        this.propel(heading);
-      } else if (heading.command === 'rotate') {
-        this.wayPoint.rotate(heading);
-      } else {
-        this.wayPoint.translate(heading);
-      }
-    });
-  }
+fleet = fleet.map(bus => {
+  return bus.setNextDepartureAfter(earliestPossibleDeparture);
+}).sort((a, b) => {
+  return a.departsAt - b.departsAt;
+})
 
-  propel(heading) {
-    this.distances['E'] += heading.distance * this.wayPoint.distances.E;
-    this.distances['N'] += heading.distance * this.wayPoint.distances.N;
-    this.distances['W'] += heading.distance * this.wayPoint.distances.W;
-    this.distances['S'] += heading.distance * this.wayPoint.distances.S;
-  }
+const bus = fleet[0];
+const solution = bus.waitTime() * bus.id;
 
-  manhattanDistance() {
-    return Math.abs(this.distances['N'] - this.distances['S'])
-      + Math.abs(this.distances['E'] - this.distances['W'])
-  }
-}
-
-const headings = input.map(heading => {
-  const direction = heading.substr(0, 1);
-  const distance = parseInt(heading.substr(1));
-  const methods = {
-    'E': 'translate',
-    'N': 'translate',
-    'W': 'translate',
-    'S': 'translate',
-    'R': 'rotate',
-    'L': 'rotate',
-    'F': 'propel',
-  };
-  const multiplier = direction === 'R' ? -1 : 1;
-  return {
-    command: methods[direction],
-    direction,
-    distance,
-    multiplier,
-  }
-});
-
-const ship = new Ship(headings);
-ship.sail();
-
-console.log('main.js@:24', ship.manhattanDistance());
+console.log('main.js@:24', solution);
