b70a185b12356a762b5ea69cc667872224c06bc4
adaviloper <16654653+adaviloper@users.noreply.github.com>
2023-12-09 20:08:38 -0600

    helper function updates

--- 2023/utils/utils.lua
+++ 2023/utils/utils.lua
@@ -1,14 +1,92 @@
 local M = {}
-function M.dump(o)
+function M.dump(o, indent)
+   indent = indent or ''
    if type(o) == 'table' then
-      local s = '{ '
+      local s = '{\n'
       for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. M.dump(v) .. ','
+         if type(k) ~= 'number' then k = k end
+         s = s .. indent .. '  ["'..k..'"] = ' .. M.dump(v, indent..'  ') .. ',\n'
       end
-      return s .. '} '
+      s = s .. indent .. '}'
+      return s
    else
-      return tostring(o)
+      if type(o) == 'number' then
+         return o
+      elseif type(o) == 'boolean' then
+         if o == true then
+            return 'true'
+         else
+            return 'false'
+         end
+      else
+         return '"'..tostring(o)..'"'
+      end
    end
 end
+
+function M.keyBy(input, keyName)
+   keyName = keyName or nil
+   local t = {}
+   for k = 1, #input do
+      if keyName == nil then
+         if t[input[k]] ~= nil then
+            print('', 'duplicate number', t[input[k]])
+         end
+         t[input[k]] = input[k]
+      else
+         table.insert(t, input[keyName], k)
+      end
+   end
+   return t
+end
+
+function M.split(input, separator)
+  local t = {}
+  for str in string.gmatch(input, '[^' .. separator .. ']+') do
+    table.insert(t, str)
+  end
+  return t
+end
+local t = {
+   ["1"] = {
+
+      ["1"] = "4",
+      ["2"] = "6",
+      ["3"] = "7",
+      ["4"] = " ",
+      ["5"] = " ",
+      ["6"] = "1",
+      ["7"] = "1",
+      ["8"] = "4",
+      ["9"] = " ",
+      ["10"] = " ",
+   },
+   ["2"] = {
+
+      ["1"] = " ",
+      ["2"] = " ",
+      ["3"] = " ",
+      ["4"] = "*",
+      ["5"] = " ",
+      ["6"] = " ",
+      ["7"] = " ",
+      ["8"] = " ",
+      ["9"] = " ",
+      ["10"] = " ",
+   },
+   ["3"] = {
+
+      ["1"] = " ",
+      ["2"] = " ",
+      ["3"] = "3",
+      ["4"] = "5",
+      ["5"] = " ",
+      ["6"] = " ",
+      ["7"] = "6",
+      ["8"] = "3",
+      ["9"] = "3",
+      ["10"] = " ",
+   },
+}
+
 return M
